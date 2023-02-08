33dd758ad98936fac983618136dd37eaf72407c4
Samuel Duque Porras <98342608+dux14@users.noreply.github.com>
2023-02-08 12:51:53 -0500

    Update mano_robotica.ino

--- mano_robotica.ino
+++ mano_robotica.ino
@@ -1,8 +1,8 @@
 #include <Servo.h>
 
-// header for controller servo
+// inicialicacion de los servos
 
-Servo servo; //keeping name of servo SERVO itself
+Servo servo; 
 
 int sensorvalue =0;
 int sensorvalue1 =0;
@@ -14,19 +14,19 @@ void setup()
 
 {
 
-            pinMode(A0,INPUT);// voltage divider value input
+            pinMode(A0,INPUT);// sensores como inputs
 			pinMode(A1,INPUT);
   			pinMode(A2,INPUT);
   			pinMode(A3,INPUT);
   			pinMode(A4,INPUT);
   
-            pinMode(3,OUTPUT);// PWM output to servo
+            pinMode(3,OUTPUT);// servos en cada pin
 			pinMode(4,OUTPUT);
   			pinMode(5,OUTPUT);
   			pinMode(6,OUTPUT);
   			pinMode(7,OUTPUT);
   
-            servo.attach(3);// telling where signal pin of servo attached(must be a PWM pin)
+            servo.attach(3);// union de servo a cada sensor
 			servo.attach(4);
   			servo.attach(5);
   			servo.attach(6);
@@ -37,13 +37,13 @@ void loop()
 
 {
 
-            sensorvalue = analogRead(A0); //read analog value from sensor
+            sensorvalue = analogRead(A0); //read analog como valor de cada sensor
 			sensorvalue1 = analogRead(A1);
   			sensorvalue2 = analogRead(A2);
   			sensorvalue3 = analogRead(A3);
   			sensorvalue4 = analogRead(A4);
   
-            servo.write((sensorvalue-250)/2); //to avoid initial positioning of servo we need to neutralize the default voltage provided by voltage divider( setting servo position based on ADC result)
+            servo.write((sensorvalue-250)/2); //determinar la posicion incial de cada servo se divide entre 2 para que quede en el lugar exacto de arranque
 			servo.write((sensorvalue1-250)/2);
   			servo.write((sensorvalue2-250)/2);
   			servo.write((sensorvalue3-250)/2);
